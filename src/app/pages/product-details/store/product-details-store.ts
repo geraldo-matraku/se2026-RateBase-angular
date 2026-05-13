ba86725e01580b7a2df9abf25b1f21b37e3cf5b6
geraldo-matraku <geraldomatraku05@gmail.com>
2026-05-13 20:15:03 +0200

    created product details store for state management

--- src/app/pages/product-details/store/product-details-store.ts
+++ src/app/pages/product-details/store/product-details-store.ts
@@ -1,8 +1,71 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import { ComponentStore } from '@ngrx/component-store';
+import { forkJoin, switchMap, tap } from 'rxjs';
 
-@Injectable({
-  providedIn: 'root',
-})
-export class ProductDetailsStore {
-  
+import { ProductDetailsService } from '../services/product-details-service';
+import { ReviewsService } from '../../reviews/services/reviews-service';
+
+export interface ProductDetailsState {
+  product: any | null;
+  reviews: any[];
+  loading: boolean;
+  error: string | null;
+}
+
+@Injectable({ providedIn: 'root' })
+export class ProductDetailsStore extends ComponentStore<ProductDetailsState> {
+  private productService = inject(ProductDetailsService);
+  private reviewService = inject(ReviewsService);
+
+  constructor() {
+    super({
+      product: null,
+      reviews: [],
+      loading: false,
+      error: null,
+    });
+  }
+
+  readonly product$ = this.select((state) => state.product);
+
+  readonly reviews$ = this.select((state) => state.reviews);
+
+  readonly loading$ = this.select((state) => state.loading);
+
+  readonly error$ = this.select((state) => state.error);
+
+  readonly fetchProductDetails = this.effect<number>((productId$) =>
+    productId$.pipe(
+      tap(() => {
+        this.patchState({
+          loading: true,
+          error: null,
+        });
+      }),
+
+      switchMap((productId) =>
+        forkJoin({
+          product: this.productService.getProductById(productId),
+          reviews: this.reviewService.getReviewsByProductId(productId),
+        }).pipe(
+          tap({
+            next: ({ product, reviews }) => {
+              this.patchState({
+                product: product.product,
+                reviews: reviews.data,
+                loading: false,
+              });
+            },
+
+            error: () => {
+              this.patchState({
+                loading: false,
+                error: 'Failed to fetch product details',
+              });
+            },
+          }),
+        ),
+      ),
+    ),
+  );
 }
