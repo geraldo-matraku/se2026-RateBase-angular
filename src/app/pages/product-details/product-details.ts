456a5f4188e7dde78d502d663a04d875197d636f
geraldo-matraku <geraldomatraku05@gmail.com>
2026-05-13 20:16:03 +0200

    implement product details page

--- src/app/pages/product-details/product-details.ts
+++ src/app/pages/product-details/product-details.ts
@@ -1,11 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProductDetailsStore } from './store/product-details-store';
+import { AsyncPipe, DatePipe } from '@angular/common';
+import { map, tap } from 'rxjs';
+import { RoleDirective } from '../../shared/directives/role-directive';
+import { CategoriesService } from '../categories/services/categoriesService';
 
 @Component({
   selector: 'app-product-details',
-  imports: [],
+  imports: [AsyncPipe, DatePipe, RoleDirective],
   templateUrl: './product-details.html',
   styleUrl: './product-details.scss',
 })
 export class ProductDetails {
+  private activatedRoute = inject(ActivatedRoute);
+  private categoriesService = inject(CategoriesService);
 
+  readonly store = inject(ProductDetailsStore);
+
+  product$ = this.store.product$;
+
+  reviews$ = this.store.reviews$;
+
+  loading$ = this.store.loading$;
+  imgBase = this.categoriesService.uploadUrl;
+
+  ngOnInit(): void {
+    this.activatedRoute.params
+      .pipe(
+        tap((data) => console.log(data)),
+        map((params) => params['prodId']),
+        map((id) => Number(id)),
+      )
+      .subscribe((prodId) => {
+        if (prodId) {
+          this.store.fetchProductDetails(prodId);
+        }
+      });
+  }
 }
--- src/app/pages/product-details/services/product-details-service.ts
+++ src/app/pages/product-details/services/product-details-service.ts
@@ -1,8 +1,15 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { environments } from '../../../../environments/environments';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProductDetailsService {
-  
+  private http = inject(HttpClient);
+
+  getProductById(productId: number): Observable<any> {
+    return this.http.get(`${environments.apiUrl}products/${productId}`);
+  }
 }
