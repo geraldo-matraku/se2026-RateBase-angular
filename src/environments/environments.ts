24b17c92f6b66bad7f03ff22a95d5e733729e9cf
geraldo-matraku <geraldomatraku05@gmail.com>
2026-05-30 15:08:06 +0200

    configurations for prod

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,5 +1,5 @@
 export const environments = {
-  apiUrl: 'http://localhost/sistem-vleresimi-produktesh-php/api/',
+  apiUrl: 'https://ratebase.infinityfreeapp.com/',
   paddle: {
     environment: 'sandbox' as const,
     clientToken: 'test_cdd3510ccb30f6205222e640a8f',
